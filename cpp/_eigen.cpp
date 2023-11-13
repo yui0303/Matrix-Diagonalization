728e2a3dbc08c11a20651d486297915cd2d01325
yuchang <iampeter62@gmail.com>
2023-11-13 16:52:44 +0800

    feat: Add files related to eigen

---
+++ cpp/_eigen.cpp
@@ -0,0 +1,24 @@
+#include "_eigen.hpp"
+
+std::pair<Matrix, Matrix> QR_decomposition(Matrix const& mat)
+{
+    Matrix Q = gram_schmidt(mat);
+    Matrix R = Q.transpose() * mat;
+    return {Q, R};
+}
+
+std::vector<double> find_eigenvalue(Matrix const& mat)
+{
+    std::pair<Matrix, Matrix> QR = QR_decomposition(mat);
+    Matrix A = QR.second * QR.first; // R*Q gives the next matrix in the sequence
+    std::vector<double> eigenvalues;
+    for (size_t i = 0; i < A.nrow(); i++) {
+        eigenvalues.push_back(A(i, i)); // The eigenvalues are the diagonal elements
+    }
+    return eigenvalues;
+}
+
+std::vector<double> find_eigenvector(Matrix const& mat, double eigenvalue)
+{
+    return {};
+}
