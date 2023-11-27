faefdfb8efd16f71c12b4b77cc20c078ba6bad02
yuchang <iampeter62@gmail.com>
2023-11-27 08:37:31 +0800

    feat: Update _eigen files
    1. Add a skeleton of QR_decomposition using householder

--- cpp/_eigen.cpp
+++ cpp/_eigen.cpp
@@ -1,15 +1,22 @@
 #include "_eigen.hpp"
 
-std::pair<Matrix, Matrix> QR_decomposition(Matrix const& mat)
+std::pair<Matrix, Matrix> QR_decomposition_GS(Matrix const& mat)
 {
     Matrix Q = gram_schmidt(mat);
     Matrix R = Q.transpose() * mat;
     return {Q, R};
 }
 
+std::pair<Matrix, Matrix> QR_decomposition_HS(Matrix const& mat)
+{
+    Matrix Q = householder(mat);
+    Matrix R = Q.transpose() * mat;
+    return {Q, R};
+}
+
 std::vector<double> find_eigenvalue(Matrix const& mat)
 {
-    std::pair<Matrix, Matrix> QR = QR_decomposition(mat);
+    std::pair<Matrix, Matrix> QR = QR_decomposition_GS(mat);
     Matrix A = QR.second * QR.first; // R*Q gives the next matrix in the sequence
     std::vector<double> eigenvalues;
     for (size_t i = 0; i < A.nrow(); i++) {
--- cpp/_eigen.hpp
+++ cpp/_eigen.hpp
@@ -4,7 +4,7 @@
 #include "_matrix.hpp"
 #include "_operation.hpp"
 
-std::pair<Matrix, Matrix> QR_decomposition(Matrix const& mat);
+std::pair<Matrix, Matrix> QR_decomposition_GS(Matrix const& mat);
 std::vector<double> find_eigenvalue(Matrix const& mat);
 std::vector<double> find_eigenvector(Matrix const& mat, double eigenvalue);
 
